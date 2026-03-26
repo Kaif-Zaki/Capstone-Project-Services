994d291e7808661b405e619da8340b4020f8ec34
Kaif-Zakey <kaifzakey22@gmail.com>
2026-03-27 00:05:20 +0530

    cloud sql proxy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   apps : [
     {
+      name   : "cloud-sql-auth-proxy",
+      script : "./cloud-sql-proxy zaki-01-486806:asia-southeast1:mysql-vm zaki-01-486806:asia-southeast1:postgres --private-ip",
+      log_file: "./logs/cloud-sql-proxy.log",
+    },
+    {
       name   : "student-service",
       script : "java -jar student-service/target/Student-Service-1.0.0.jar",
       log_file: "./logs/student-service.log",
